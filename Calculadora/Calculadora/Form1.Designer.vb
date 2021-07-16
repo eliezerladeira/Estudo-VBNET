c1f0f8d4f94cd80076a2183a3e5aace63dd7cfeb
eliezerladeira <eliezerladeira@yahoo.com.br>
2021-07-16 10:32:44 -0300

    Calculadora Natural - Soma

--- Calculadora/Calculadora/Form1.Designer.vb
+++ Calculadora/Calculadora/Form1.Designer.vb
@@ -23,14 +23,14 @@ Partial Class Form1
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Me.grpCalculadora = New System.Windows.Forms.GroupBox()
-        Me.lblValor1 = New System.Windows.Forms.Label()
-        Me.txtValor1 = New System.Windows.Forms.TextBox()
+        Me.lblSomar = New System.Windows.Forms.Label()
+        Me.lblResultado = New System.Windows.Forms.Label()
+        Me.txtResultado = New System.Windows.Forms.TextBox()
+        Me.btnSomar = New System.Windows.Forms.Button()
         Me.txtValor2 = New System.Windows.Forms.TextBox()
         Me.lblValor2 = New System.Windows.Forms.Label()
-        Me.Button1 = New System.Windows.Forms.Button()
-        Me.txtResultado = New System.Windows.Forms.TextBox()
-        Me.lblResultado = New System.Windows.Forms.Label()
-        Me.lblSomar = New System.Windows.Forms.Label()
+        Me.txtValor1 = New System.Windows.Forms.TextBox()
+        Me.lblValor1 = New System.Windows.Forms.Label()
         Me.Label1 = New System.Windows.Forms.Label()
         Me.grpCalculadora.SuspendLayout()
         Me.SuspendLayout()
@@ -41,33 +41,53 @@ Partial Class Form1
         Me.grpCalculadora.Controls.Add(Me.lblSomar)
         Me.grpCalculadora.Controls.Add(Me.lblResultado)
         Me.grpCalculadora.Controls.Add(Me.txtResultado)
-        Me.grpCalculadora.Controls.Add(Me.Button1)
+        Me.grpCalculadora.Controls.Add(Me.btnSomar)
         Me.grpCalculadora.Controls.Add(Me.txtValor2)
         Me.grpCalculadora.Controls.Add(Me.lblValor2)
         Me.grpCalculadora.Controls.Add(Me.txtValor1)
         Me.grpCalculadora.Controls.Add(Me.lblValor1)
         Me.grpCalculadora.Location = New System.Drawing.Point(12, 12)
         Me.grpCalculadora.Name = "grpCalculadora"
-        Me.grpCalculadora.Size = New System.Drawing.Size(510, 82)
+        Me.grpCalculadora.Size = New System.Drawing.Size(510, 94)
         Me.grpCalculadora.TabIndex = 0
         Me.grpCalculadora.TabStop = False
         Me.grpCalculadora.Text = "Soma"
         '
-        'lblValor1
+        'lblSomar
         '
-        Me.lblValor1.AutoSize = True
-        Me.lblValor1.Location = New System.Drawing.Point(33, 28)
-        Me.lblValor1.Name = "lblValor1"
-        Me.lblValor1.Size = New System.Drawing.Size(40, 13)
-        Me.lblValor1.TabIndex = 0
-        Me.lblValor1.Text = "Valor 1"
+        Me.lblSomar.AutoSize = True
+        Me.lblSomar.Font = New System.Drawing.Font("Microsoft Sans Serif", 16.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblSomar.Location = New System.Drawing.Point(119, 44)
+        Me.lblSomar.Name = "lblSomar"
+        Me.lblSomar.Size = New System.Drawing.Size(25, 26)
+        Me.lblSomar.TabIndex = 7
+        Me.lblSomar.Text = "+"
         '
-        'txtValor1
+        'lblResultado
         '
-        Me.txtValor1.Location = New System.Drawing.Point(36, 44)
-        Me.txtValor1.Name = "txtValor1"
-        Me.txtValor1.Size = New System.Drawing.Size(69, 20)
-        Me.txtValor1.TabIndex = 1
+        Me.lblResultado.AutoSize = True
+        Me.lblResultado.Location = New System.Drawing.Point(367, 28)
+        Me.lblResultado.Name = "lblResultado"
+        Me.lblResultado.Size = New System.Drawing.Size(55, 13)
+        Me.lblResultado.TabIndex = 6
+        Me.lblResultado.Text = "Resultado"
+        '
+        'txtResultado
+        '
+        Me.txtResultado.Location = New System.Drawing.Point(370, 44)
+        Me.txtResultado.Name = "txtResultado"
+        Me.txtResultado.Size = New System.Drawing.Size(100, 20)
+        Me.txtResultado.TabIndex = 5
+        '
+        'btnSomar
+        '
+        Me.btnSomar.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnSomar.Location = New System.Drawing.Point(261, 41)
+        Me.btnSomar.Name = "btnSomar"
+        Me.btnSomar.Size = New System.Drawing.Size(75, 23)
+        Me.btnSomar.TabIndex = 4
+        Me.btnSomar.Text = "="
+        Me.btnSomar.UseVisualStyleBackColor = True
         '
         'txtValor2
         '
@@ -85,46 +105,26 @@ Partial Class Form1
         Me.lblValor2.TabIndex = 2
         Me.lblValor2.Text = "Valor 2"
         '
-        'Button1
+        'txtValor1
         '
-        Me.Button1.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Button1.Location = New System.Drawing.Point(264, 42)
-        Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(75, 23)
-        Me.Button1.TabIndex = 4
-        Me.Button1.Text = "="
-        Me.Button1.UseVisualStyleBackColor = True
+        Me.txtValor1.Location = New System.Drawing.Point(36, 44)
+        Me.txtValor1.Name = "txtValor1"
+        Me.txtValor1.Size = New System.Drawing.Size(69, 20)
+        Me.txtValor1.TabIndex = 1
         '
-        'txtResultado
+        'lblValor1
         '
-        Me.txtResultado.Location = New System.Drawing.Point(370, 44)
-        Me.txtResultado.Name = "txtResultado"
-        Me.txtResultado.Size = New System.Drawing.Size(100, 20)
-        Me.txtResultado.TabIndex = 5
-        '
-        'lblResultado
-        '
-        Me.lblResultado.AutoSize = True
-        Me.lblResultado.Location = New System.Drawing.Point(367, 28)
-        Me.lblResultado.Name = "lblResultado"
-        Me.lblResultado.Size = New System.Drawing.Size(55, 13)
-        Me.lblResultado.TabIndex = 6
-        Me.lblResultado.Text = "Resultado"
-        '
-        'lblSomar
-        '
-        Me.lblSomar.AutoSize = True
-        Me.lblSomar.Font = New System.Drawing.Font("Microsoft Sans Serif", 16.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.lblSomar.Location = New System.Drawing.Point(119, 39)
-        Me.lblSomar.Name = "lblSomar"
-        Me.lblSomar.Size = New System.Drawing.Size(25, 26)
-        Me.lblSomar.TabIndex = 7
-        Me.lblSomar.Text = "+"
+        Me.lblValor1.AutoSize = True
+        Me.lblValor1.Location = New System.Drawing.Point(33, 28)
+        Me.lblValor1.Name = "lblValor1"
+        Me.lblValor1.Size = New System.Drawing.Size(40, 13)
+        Me.lblValor1.TabIndex = 0
+        Me.lblValor1.Text = "Valor 1"
         '
         'Label1
         '
         Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(9, 109)
+        Me.Label1.Location = New System.Drawing.Point(9, 121)
         Me.Label1.Name = "Label1"
         Me.Label1.Size = New System.Drawing.Size(39, 13)
         Me.Label1.TabIndex = 1
@@ -135,7 +135,7 @@ Partial Class Form1
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(128, Byte), Integer))
-        Me.ClientSize = New System.Drawing.Size(536, 450)
+        Me.ClientSize = New System.Drawing.Size(536, 162)
         Me.ControlBox = False
         Me.Controls.Add(Me.Label1)
         Me.Controls.Add(Me.grpCalculadora)
@@ -153,7 +153,7 @@ Partial Class Form1
     Friend WithEvents lblSomar As Label
     Friend WithEvents lblResultado As Label
     Friend WithEvents txtResultado As TextBox
-    Friend WithEvents Button1 As Button
+    Friend WithEvents btnSomar As Button
     Friend WithEvents txtValor2 As TextBox
     Friend WithEvents lblValor2 As Label
     Friend WithEvents txtValor1 As TextBox
--- Calculadora/Calculadora/Form1.vb
+++ Calculadora/Calculadora/Form1.vb
@@ -1,3 +1,20 @@
 ﻿Public Class Form1
+    Private Sub btnSomar_Click(sender As Object, e As EventArgs) Handles btnSomar.Click
+        Dim valor1 As Integer = 0
+        Dim valor2 As Integer = 0
 
+        valor1 = CInt(txtValor1.Text)
+        valor2 = CInt(txtValor2.Text)
+
+        Soma(valor1, valor2)
+        'txtResultado.Text = CInt(txtValor1.Text) + CInt(txtValor2.Text)
+    End Sub
+
+    Private Sub Soma(ByVal v1 As Integer, ByVal v2 As Integer)
+        Dim resultado As Integer
+
+        resultado = v1 + v2
+
+        txtResultado.Text = resultado
+    End Sub
 End Class
